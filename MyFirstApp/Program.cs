0ef992651c80eea343541c62c2de3ad6b47389a0
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-03-20 19:58:58 -0300

    Adicionei um novo texto

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -6,7 +6,7 @@ class Program
     static void Main(string[] args) 
     {
         Console.WriteLine("Hello World!");
-        
+        Console.Write("Enzo");
         Console.WriteLine("Did you enjoy your break ?");
     }
 }
